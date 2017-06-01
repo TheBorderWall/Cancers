67870e8999aee1a9c612fdcbc7f6ce9d37630693
chestor21 <chestormaster21@gmail.com>
2017-06-01 08:03:15 -0400

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -1,10 +1,28 @@
-class Demo 
+import java.util.Scanner;
+
+class Codes
 {
   public static void main (String[] args)
   {
 //here's a comment
     //* and here's another comment //*
+ 
+    
+
+
+
+public static void main(String [] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a sentence: ");
+    String sentence = input.nextLine();
+    String[] words = sentence.split(" ");
+    for (int i = words.length - 1; i >= 0; i--) {
+    System.out.print(" "+ words[i]);
+
+    }
 }
+ }
+  }
 
 
 
