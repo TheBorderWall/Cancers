0085139add809eda104236ab28849ba970690bb8
chestor21 <chestormaster21@gmail.com>
2017-06-01 08:04:21 -0400

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -8,9 +8,29 @@ class Codes
     //* and here's another comment //*
  
     
-
-
-
+//chestor code 1
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        String result = "";
+        char firstChar = input.charAt(0);
+        result = result + Character.toUpperCase(firstChar);
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char previousChar = input.charAt(i - 1);
+            if (previousChar == ' ') {
+                result = result + Character.toUpperCase(currentChar);
+            } else {
+                result = result + currentChar;
+            }
+        }
+        System.out.println(result);
+    }
+}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Sec
+//chestor code 2
 public static void main(String [] args) {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter a sentence: ");
